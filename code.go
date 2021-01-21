d7b24ded59c04bc288d485b508e20438fcf2772e
Warren, Ben <bwarren@eab.com>
2021-01-20 23:45:03 -0500

    Why is this broken?

--- code.go
+++ code.go
@@ -1,79 +1,33 @@
 package advent20201223
 
-// Part1 solves part1
-func Permute(input []int, times int) []int {
-	for i := 0; i < times; i++ {
-		index := i % len(input)
-		current := index
-		currentValue := input[index]
-		// fmt.Println("Input:", input)
-		// fmt.Println("Index:", index)
-		// fmt.Println("Current:", current)
-		removed := []int{}
-		for j := 1; j < 4; j++ {
-			removed = append(removed, input[(i+j)%len(input)])
-		}
-		// fmt.Println("pick up:", removed)
-		input = OtherNumbers(input, removed)
-		// fmt.Println("remaining:", input)
-		insertIndex := IndexForCurrentCup(input, currentValue)
-		// fmt.Println("insert at:", insertIndex)
-		piece1 := append([]int{}, input[:insertIndex]...)
-		piece2 := append([]int{}, input[insertIndex:]...)
-		input = append(piece1, removed...)
-		input = append(input, piece2...)
-		// fmt.Println("Produces:", input)
-		input = RotateToAssert(input, current, currentValue)
-		// fmt.Println("Rotated:", input)
-	}
-	return input
+import "fmt"
+
+type Node struct {
+	ID   int
+	Next *Node
 }
 
-func OtherNumbers(input, exclude []int) (result []int) {
-	for _, value := range input {
-		if !Contains(exclude, value) {
-			result = append(result, value)
+func GenerateRing(seed []int, ct int) (Node, map[int]Node) {
+	nodeMap := make(map[int]Node)
+	firstNode := Node{ID: seed[0]}
+	firstNodeAddress := &firstNode
+	var appendTo Node
+	appendTo = firstNode
+	var value int
+	for i := 1; i < ct; i++ {
+		if i < len(seed) {
+			value = seed[i]
+		} else {
+			value = i
 		}
-	}
-	return
-}
+		newNode := Node{ID: value}
+		appendTo.Next = &newNode
+		fmt.Printf("%p %v \n", &appendTo, appendTo)
 
-func Contains(lst []int, target int) bool {
-	for _, value := range lst {
-		if value == target {
-			return true
-		}
+		appendTo = newNode
 	}
-	return false
-}
-
-func IndexForCurrentCup(input []int, value int) int {
-	for i := 1; i < 4; i++ {
-		target := value - i
-		if target <= 0 {
-			target += len(input) + 3
-		}
-		for idx, value := range input {
-			if value == target {
-				return idx + 1
-			}
-		}
-	}
-	return 0
-}
-
-func RotateToAssert(input []int, idx, value int) []int {
-	for input[idx] != value {
-		input = append(input[1:], input[0])
-	}
-	return input
-}
-
-func IndexOf(data []int, target int) int {
-	for k, v := range data {
-		if target == v {
-			return k
-		}
-	}
-	return -1
+	fmt.Println(appendTo)
+	appendTo.Next = firstNodeAddress
+	fmt.Println(appendTo)
+	return *(appendTo.Next), nodeMap
 }
--- code_test.go
+++ code_test.go
@@ -6,27 +6,15 @@ import (
 	"testing"
 )
 
-func TestSample(t *testing.T) {
-	fmt.Println(advent.Permute([]int{3, 8, 9, 1, 2, 5, 4, 6, 7}, 10))
-	t.Fail()
-}
-func TestPart1(t *testing.T) {
-	fmt.Println(advent.Permute([]int{6, 2, 4, 3, 9, 7, 1, 5, 8}, 100))
-	t.Fail()
-}
 func TestPart2(t *testing.T) {
 	firstFew := []int{6, 2, 4, 3, 9, 7, 1, 5, 8}
-	input := make([]int, 1000000)
-	for i := 1; i < 1000001; i++ {
-		if i < 10 {
-			input[i-1] = firstFew[i-1]
-		} else {
-			input[i-1] = i
-		}
+	firstNode, nodeMap := advent.GenerateRing(firstFew, 9)
+	// next := firstNode.Next
+	fmt.Println(firstNode)
+	// next = next.Next
+	for i := 0; i <= 9; i++ {
 	}
-	ints := advent.Permute(input, 1000)
-	onePlace := advent.IndexOf(ints, 1)
-	fmt.Println(onePlace, ints[onePlace:onePlace+3], ints[:2])
+	fmt.Println(nodeMap)
 	t.Fail()
 }
 
